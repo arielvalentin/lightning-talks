7bf91e4ff162bc379956aa399b3781b23f1fa1e0
Ariel Valentin <ariel@mataleon.home>
2015-06-23 21:56:31 -0400

    changed the sample to match

--- 2015-06/inheritance/create_discussion.rb
+++ 2015-06/inheritance/create_discussion.rb
@@ -1,14 +1,16 @@
 class CreateDiscussion < Base
   def process(topic_id, group_id)
-    group_id = Group.find(group_id)
-    topic_id = Topic.find(topic_id)
-    discussion = Discussion.new(user: user,
-                                subject: email.subject,
-                                message: email_content)
+    group = Group.find(group_id)
+    discussion = group.topics.find(topic_id).
+      discussions.new(
+        user: user,
+        subject: email.subject,
+        message: email_content
+    )
 
-    if Ability.new(user).can?(:create, discussion)
-      discussion.save!
-      track_event('discussion_created', discussion)
-    end
+      if Ability.new(user).can?(:create, discussion)
+        discussion.save!
+        track_event('discussion_created', discussion)
+      end
   end
 end
